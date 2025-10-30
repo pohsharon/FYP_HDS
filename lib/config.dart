5f26f573db5f3a33519d921b15fc4f041bf0d01d
pohsharon <pohsharon8855@gmail.com>
2025-10-30 15:59:12 +0800

    done setup real device

--- lib/config.dart
+++ lib/config.dart
@@ -2,13 +2,13 @@ import 'dart:io';
 
 class Config {
   static String get apiBaseUrl {
-    if (Platform.isAndroid) {
-      return "http://10.0.2.2:8000/api"; // Android emulator uses 10.0.2.2
-    } else if (Platform.isIOS) {
-      return "http://127.0.0.1:8000/api"; // iOS simulator uses localhost
-    } else {
-      return "http://127.0.0.1:8000/api"; // Default for other platforms (Mac, Windows)
-    }
+    // if (Platform.isAndroid) {
+    //   return "http://10.0.2.2:8000/api"; // Android emulator uses 10.0.2.2
+    // } else if (Platform.isIOS) {
+    //   return "http://127.0.0.1:8000/api"; // iOS simulator uses localhost
+    // } else {
+      return "http://192.168.0.27:8000/api"; // Default for other platforms (Mac, Windows)
+    // }
   }
 
   // No change in Supabase
