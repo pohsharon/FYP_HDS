4530122d6791be7cd2b6d2bb7d23ae63490ae75c
pohsharon <pohsharon8855@gmail.com>
2026-01-05 22:58:41 +0800

    Update config

--- lib/config.dart
+++ lib/config.dart
@@ -4,10 +4,11 @@ class Config {
     // if (Platform.isAndroid) {
       // return "http://10.0.2.2:8000/api"; // Android emulator uses 10.0.2.2
     // } else if (Platform.isIOS) {
-      return "http://127.0.0.1:8000/api"; // iOS simulator uses localhost
+      // return "http://127.0.0.1:8000/api"; // iOS simulator uses localhost
     // } else {
       // return "http://192.168.0.26:8000/api"; // Default for other platforms (Mac, Windows)
     // }
+    return "https://app-hosbaduriansystem-dev-001-g5dwg4gpeqbfgqgy.southeastasia-01.azurewebsites.net/api";
   }
 
   // No change in Supabase
