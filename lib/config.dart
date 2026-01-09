bfa9881ace0f91c89a2dc25e30dba3947d1f38b8
pohsharon <pohsharon8855@gmail.com>
2026-01-09 17:52:55 +0800

    Deploy url

--- lib/config.dart
+++ lib/config.dart
@@ -4,11 +4,12 @@ class Config {
     // if (Platform.isAndroid) {
       // return "http://10.0.2.2:8000/api"; // Android emulator uses 10.0.2.2
     // } else if (Platform.isIOS) {
-      return "http://127.0.0.1:8000/api"; // iOS simulator uses localhost
+      // return "http://127.0.0.1:8000/api"; // iOS simulator uses localhost
     // } else {
-      // return "http://192.168.0.26:8000/api"; // Default for other platforms (Mac, Windows)
+      // return "http://192.168.1.20:8000/api"; // Default for other platforms (Mac, Windows)
     // }
-    // return "https://app-hosbaduriansystem-dev-001-g5dwg4gpeqbfgqgy.southeastasia-01.azurewebsites.net/api";
+    // return "http://192.168.1.20:8000/api"; //Android real device
+    return "https://app-hosbaduriansystem-dev-001-g5dwg4gpeqbfgqgy.southeastasia-01.azurewebsites.net/api";
   }
 
   // No change in Supabase
